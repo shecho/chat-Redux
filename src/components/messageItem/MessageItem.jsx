e1d92f0289f8c6d5ec47a7017a078d60a264af80
shecho <shechovala@gmail.com>
2020-08-25 21:32:41 -0500

    Added message options on on the messageItem, and created a raw container to contain message time and icons

--- src/components/messageItem/MessageItem.jsx
+++ src/components/messageItem/MessageItem.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./MessageItem.css";
-import MessageOption from "../messageOption/MessajeOption.jsx"
-// import moment from "moment";
+import MessageOption from "../messageOption/MessajeOption.jsx";
 
+
+const rowMessageStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+  width: "100%"
+};
 export default function Message() {
   return (
-    <div>
-      <h6>Hora aqui</h6>
-      <div className="bubble-container">
-        <div className="bubble">Mensaje</div>
-        <div>
-            <MessageOption/>
+    <div className ='message'>
+      <div className="bubble-container"  >
+        <div className="bubble" style={rowMessageStyles}>
+          <p>Mensaje </p>
+          <p>  Hora</p>
+          <MessageOption />
         </div>
       </div>
     </div>
--- src/components/messageOption/MessajeOption.jsx
+++ src/components/messageOption/MessajeOption.jsx
@@ -1,16 +1,19 @@
-
 import React from "react";
-import ToolbarButton from "../toolbarButton/ToolbarButton.jsx"
+import ToolbarButton from "../toolbarButton/ToolbarButton.jsx";
 
+const messageButtonStyles = {
+  display: "flex",
+  justifyContent:'flex-end'
 
+}
 
-export default function MessageOption (props) {
+export default function MessageOption(props) {
   return (
-    <div className="">
-          <ToolbarButton key="photo" icon="ios-trash-outline " />
-          <ToolbarButton key="image" icon="ios-copy-outline" />
-          <ToolbarButton key="audio" icon="ios-share-alt" />
-          <ToolbarButton key="money" icon="ios-star-outline" />
-     </div>
+    <div className="" style={messageButtonStyles}>
+      <ToolbarButton key="photo" icon="ion-ios-trash" />
+      <ToolbarButton key="image" icon="ion-ios-copy" />
+      <ToolbarButton key="audio" icon="ion-ios-share-alt" />
+      <ToolbarButton key="money" icon="ion-ios-star" />
+    </div>
   );
 }
